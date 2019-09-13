5524bdf89c3812b0d74c0b146f19e783c6ef8862
ana <anapntra@gmail.com>
2019-09-13 09:56:37 -0300

    congirurar Evento onclick com style inline e css

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 let menu = document.querySelector(".menu"); 
+let nav= document.querySelector("nav");
+// Evento
+
+
+function mostrarMenu(){ //nesse caso nao precisa declarar nemhum parametro no parenteses pq nao vamos inserir nenhum valor 
+    // nav.style.display="block"; //dessa forma, usando o style direto do HTML, ele vai colocar as proriedades direto no html, tipo um css inline, que tem a maior prioridade
+    nav.classList.add("visivel"); //aqui ele puxa pelo CSS e diciona uma class no momento do evento.
+}
+
+menu.onclick =mostrarMenu;
