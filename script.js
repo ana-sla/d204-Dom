1344608ecf7f2d4bdf59c2fdc791ba759ec43874
ana <anapntra@gmail.com>
2019-09-13 10:17:55 -0300

    fazer menu aparecer e desaparecer usando toggle

--- script.js
+++ script.js
@@ -4,15 +4,22 @@ let nav= document.querySelector("nav");
 
 
 function mostrarMenu(){ //nesse caso nao precisa declarar nemhum parametro no parenteses pq nao vamos inserir nenhum valor 
+//INLINE HTML
     // nav.style.display="block"; //dessa forma, usando o style direto do HTML, ele vai colocar as proriedades direto no html, tipo um css inline, que tem a maior prioridade
+//CSS    
     //nav.classList.add("visivel"); //aqui ele puxa pelo CSS e diciona uma class no momento do evento.
 
-    if (nav.classList.contains("visivel")){
-        nav.classList.remove("visivel");
-     } else{
-        nav.classList.add("visivel");
-    } // classlist = consulta e faz mudanças dentro de todos os elementos de uma class. 
-      //aqui estamos fazendo uma condição dentro do onclick, para fazer aparecer de desaparecer a class 'visivel' no html e funcionar o menu     
+//JEITO 1
+    // if (nav.classList.contains("visivel")){
+    //     nav.classList.remove("visivel");
+    //  } else{
+    //     nav.classList.add("visivel");
+    // } // classlist = consulta e faz mudanças dentro de todos os elementos de uma class. 
+      //aqui estamos fazendo uma condição dentro do onclick, para fazer aparecer de desaparecer a class 'visivel' no html e funcionar o menu  
+      
+ //JEITO 2
+ 
+    nav.classList.toggle ("visivel"); //o toggle ja faz a alternancia no hmtl automaticamente
 
 }
 
